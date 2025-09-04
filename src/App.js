21a82bd294d3aec98f7cec9cd8e0be7fce7d4295
greemaltend4 <dengestabiy@hotmail.com>
2025-09-04 12:45:39 -0500

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Bienvenue dans l'application de gestion de documents</h1>
+    </div>
+  );
+}
+
+export default App;
